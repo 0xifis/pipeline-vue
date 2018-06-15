dd379105f2c6a660648bf1038465317508773c5e
Vishnu R Menon <vishthemenon@gmail.com>
2018-06-15 11:49:04 +0100

    Add acts_as_listable

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :lists
   resources :listings
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'lists#index'
 end
